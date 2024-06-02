955f7bf8ac76239d3e151352a11b65f33a002b17
Z0SO <lautarorobales31@gmail.com>
2024-06-01 21:39:52 -0300

    Agregado: Servidor ASINCRONO

--- my_ws_app/consumidor.py
+++ my_ws_app/consumidor.py
@@ -1,66 +1,73 @@
 
-# consumidor de websocket
-
-
 import json
 
-# importamos el decorador async_to_sync
-# para convertir funciones asincronas en sincronas
-from asgiref.sync import async_to_sync
-
-# importamos la clase WebsocketConsumer
-from channels.generic.websocket import WebsocketConsumer
+# importamos la clase AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
+class ChatConsumer(AsyncWebsocketConsumer):
 
-class ChatConsumer(WebsocketConsumer):
+    # metodo que se ejecuta cuando un cliente se conecta al servidor
+    async def connect(self):
 
-    # metodo para conectarse al grupo de chat
-    def connect(self):
         # obtenemos el nombre de la sala
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        
-        # creamos el nombre del grupo de chat
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]        
         self.room_group_name = f"chat_{self.room_name}"
 
-        # nos unimos al grupo de chat
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-
+        # unimos al cliente a la sala
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         # aceptamos la conexion
-        self.accept()
+        await self.accept()
 
 
-    # metodo para desconectarse del grupo de chat
-    def disconnect(self, close_code):
+    # metodo que se ejecuta cuando un cliente se desconecta del servidor
+    async def disconnect(self, close_code):
 
-        # nos salimos del grupo de chat
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+        # sacamos al cliente de la sala
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    # metodo para recibir mensajes del cliente, luego los envia al grupo de chat
-    def receive(self, text_data):
 
-        # convertimos el mensaje a json        
+    # metodo que se ejecuta cuando el servidor recibe un mensaje del cliente
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-
-        # obtenemos el mensaje
         message = text_data_json["message"]
+        browser = text_data_json["browser"]
 
-        # enviamos el mensaje al grupo de chat
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
+        # Ahora puedes usar 'browser' como quieras. Por ejemplo, podrías enviarlo de vuelta al cliente:
+        await self.channel_layer.group_send(
+            self.room_group_name, {
+                "type": "chat.message",
+                "message": message,
+                "browser": browser
+            }
         )
 
 
-    # metodo para enviar mensajes, recibe del grupo de chat y los manda al websocket
-    def chat_message(self, event):
+    # metodo que se ejecuta cuando el servidor envia un mensaje a la sala
+    async def chat_message(self, event):
 
         # obtenemos el mensaje
         message = event["message"]
+        browser = event["browser"]
 
-        # enviamos el mensaje al websocket
-        self.send(text_data=json.dumps({"message": message}))
+        # enviamos el mensaje al cliente a través del websocket
+        await self.send(text_data=json.dumps(
+            {"message": message, "browser": browser}
+        ))
+
+
+
+'''
+
+self.room_name: 
+    Es el nombre de la sala de chat a la que el cliente se une.
+    Se obtiene de la URL de la solicitud WebSocket.
+
+self.room_group_name: 
+    Es el nombre del grupo de chat. 
+    Se crea a partir del room_name. 
+    En Django Channels, un grupo es un canal
+    que puede tener múltiples oyentes.
+    
+'''
